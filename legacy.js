443fb8fe42c6eae956d181e96c46093bd2ea6e5d
Ivan Lyah <johnnye.be@gmail.com>
2016-03-09 20:47:39 +0200

    initial commit

---
+++ legacy.js
@@ -0,0 +1,23 @@
+module.exports = {
+
+    extends: [
+        './rules/best-practices',
+        './rules/errors',
+        './rules/legacy',
+        './rules/node',
+        './rules/style',
+        './rules/variables'
+    ].map(require.resolve),
+
+    env: {
+        browser: true,
+        node   : true,
+        amd    : true,
+        mocha  : true,
+        jasmine: true
+    },
+
+    ecmaFeatures: {},
+    globals     : {},
+    rules       : {}
+};
